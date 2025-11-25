245f10e2337808582fcd3cf61959a3f00beac502
StevenChang21 <changstevenn@gmail.com>
2025-11-25 11:33:34 +0800

    config: update Package ID for Sui Devnet deployment

---
+++ frontend-react/src/chainConfig.js
@@ -0,0 +1,3 @@
+// After deploying with 'one client publish', paste your Package ID here
+export const PACKAGE_ID = "0x397b8e42ceb5943e207284f31b4bfce62a76341af57488b555382c2ce940c0be";
+export const MODULE_NAME = "game_core";
