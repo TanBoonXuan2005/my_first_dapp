9cdf13e2c45ae139f690b609ac45b5ddfd8e1bd5
StevenChang21 <changstevenn@gmail.com>
2025-11-25 17:49:12 +0800

    fix(config): remove trailing space from package ID

--- frontend-react/src/chainConfig.js
+++ frontend-react/src/chainConfig.js
@@ -1,3 +1,3 @@
 // After deploying with 'one client publish', paste your Package ID here
-export const PACKAGE_ID = "0x8d5d5dace94a5d2ab2fe5d8b35f6e9b2f0e8cb35f00361070467508845a21b82";
+export const PACKAGE_ID = "0x5a2ce3be4d7b380fbd9c35ef6e08c7b19b8ff94296d6af9d3ff7e57eedc01fc2";
 export const MODULE_NAME = "game_core";
