89f06f40dcffc2591d18482b580cbe98da0d3eaf
StevenChang21 <changstevenn@gmail.com>
2025-11-27 21:00:10 +0800

    chore: update deployment config

--- frontend-react/src/chainConfig.js
+++ frontend-react/src/chainConfig.js
@@ -1,3 +1,5 @@
 // After deploying with 'one client publish', paste your Package ID here
-export const PACKAGE_ID = "0x6bf4338f02d906f68e79e7565807f0be5c5605ec5dea430eda71672318fd478e";
+export const PACKAGE_ID = "0x3c8d0eacd4640e02f346f483148089563d44dab255d024015efa645120dbb1d8";
 export const MODULE_NAME = "game_core";
+export const USDT_TREASURY_CAP_ID = "0xd8f560dcfed0cad52cf2e31e0b930138c0be8b4232ac918f3073af7c1b81d99e";
+export const USDT_ID = "0x3c8d0eacd4640e02f346f483148089563d44dab255d024015efa645120dbb1d8::usdt::USDT";
